8ac7dcf38b11e94a6153ecf751ce451d11171c8b
man07har <thakurmanoharkumar7@gmail.com>
2024-02-02 19:40:30 +0530

    documented

--- server.js
+++ server.js
@@ -2,13 +2,26 @@ const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+
+
+//database connecton 
 connectDb();
-app.use(express.json()); //middleware to parse the json data
-app.use("/api/contacts", require("./routes/contactRoutes")); //setting up the routes
+
+//middleware to parse the json data
+app.use(express.json()); 
+
+//setting up the routes
+app.use("/api/contacts", require("./routes/contactRoutes")); 
 app.use("/api/users", require("./routes/userRoutes"));
 
-app.use(errorHandler); //middleware to handle errors
+
+//middleware to handle errors
+app.use(errorHandler); 
+
+
+
+//connection to the port
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`server running in port ${port}`);
 });
